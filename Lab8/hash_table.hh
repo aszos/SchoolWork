7f42500c8d74906ddb7af0cef48610787de3b51d
Alex Szostek <aszostek@udel.edu>
2014-11-14 15:49:50 -0500

    seperated data structure and main file

---
+++ Lab8/hash_table.hh
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+const int DEC_TO_INT = 97;
+const int HASH_MOD = 11;
+
+class HashTable
+{
+	public:
+		HashTable(){ }
+		HashTable(int length, char** words) 
+		{
+			hash_table = generate_hash_table(length, *words);
+		}
+
+		int hash_char(char a);
+		int hash_char_array(char *a);
+		int * generate_hash_table(int argc, char* argv);
+		void print_hashed_char_array(char *a);
+		void print_hashed_parameters(int len, char * argv[]);
+		void print_hash_table();	
+	private:
+		int length;
+		int load_factor;	
+		int *hash_table;
+};
+
+
+int HashTable::hash_char(char a)
+{
+	return (((int) a) - DEC_TO_INT);	
+}
+
+int HashTable::hash_char_array(char *a)
+{
+	int hash = 0;
+
+	for(int i = 0; i < strlen(a); i++)
+	{
+		hash += hash_char(a[i]);
+	}
+
+	return (hash % HASH_MOD);
+}
+
+void HashTable::print_hashed_char_array(char *a)
+{
+	int i;
+
+	for(i = 0; i < strlen(a) - 1; i++)
+	{
+		cout << hash_char((a[i])) << " + ";
+	}
+
+	cout << hash_char(a[i]) << " (mod 11) = " << hash_char_array(a) << endl; 
+}
+
+void HashTable::print_hashed_parameters(int len, char * argv[])
+{
+	for(int i = 1; i < len; i++)
+	{
+		cout << "Char: " << argv[i] << " | "; 
+		print_hashed_char_array(argv[i]);
+	}
+}
+
+int * HashTable::generate_hash_table(int argc, char* argv)
+{
+	int * ht = new int[argc]();
+
+	return ht;
+}
+
+void HashTable::print_hash_table()
+{
+	for(int i = 0 ; i < length; i++)
+	{
+		cout << " | " << hash_table[i];	
+	}
+
+	cout << " | " << endl;
+}
--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -1,58 +1,9 @@
 #include <iostream>
 #include <cstring>
-
-using namespace std;
-const int DEC_TO_INT = 97;
-const int HASH_MOD = 11;
-
-int hash_char(char a)
-{
-	return (((int) a) - DEC_TO_INT);	
-}
-
-int hash_char_array(char *a)
-{
-	int hash = 0;
-
-	for(int i = 0; i < strlen(a); i++)
-	{
-		hash += hash_char(a[i]);
-	}
-
-	return (hash % HASH_MOD);
-}
-
-void print_hashed_array(char *a)
-{
-	int i;
-
-	for(i = 0; i < strlen(a) - 1; i++)
-	{
-		cout << hash_char((a[i])) << " + ";
-	}
- 	
-	cout << hash_char(a[i]) << " (mod 11) = " << hash_char_array(a) << endl; 
-
-}
-
-char * generate_hash_table(int len, char * argv[])
-{
-	int i; 
-	int j;
-	char * ht = new char[len]();
-
-	for(i = 1; i < len; i++)
-	{
-		cout << "Char: " << argv[i] << " | "; 
-		print_hashed_array(argv[i]);
-	}
-
-	return ht;
-}
+#include "hash_table.hh"
 
 int main(int argc, char* argv[])
 {
- 	generate_hash_table(argc, argv);
 	return 0;
 }
 
