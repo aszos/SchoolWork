b1008e54f36f57d6dec476bdcd0049c286516950
Alex Szostek <aszostek@udel.edu>
2015-02-24 15:56:06 -0500

    Removed old files

--- Undergrad/CISC275/Lab2/SimpleSimon/core/src/com/aszostek/SimpleSimon/StartScreen.java
+++
@@ -1,9 +0,0 @@
-package com.aszostek.SimpleSimon;
-
-import com.badlogic.gdx.ScreenAdapter;
-
-public class StartScreen extends ScreenAdapter
-{
-	//display name of game, start button, about button
-	//background maybe?
-}
--- Undergrad/CISC275/Lab2/SimpleSimon/core/src/com/aszostek/SimpleSimon/TryAgainScreen.java
+++
@@ -1,9 +0,0 @@
-package com.aszostek.SimpleSimon;
-
-import com.badlogic.gdx.ScreenAdapter;
-
-public class TryAgainScreen extends Screen
-{
-	//"Try Again?" or quit
-	//black background, white text
-}
